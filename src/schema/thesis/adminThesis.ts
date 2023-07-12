22c3275f937940e47265efd4a6e9958a2c89e473
emon5122 <emon5122@gmail.com>
2023-07-13 01:52:11 +0600

    evaluators fix

--- src/schema/thesis/adminThesis.ts
+++ src/schema/thesis/adminThesis.ts
@@ -5,9 +5,17 @@ export const thesisBody = z.object({
     studentId: z.string().uuid(),
     supervisorId: z.string().uuid()
 })
-export const teachersBody= z.array(z.object({id:z.string().uuid()}))
+export const teachersBody= z.array(z.string().uuid())
 
 export const thesisEvaluators = z.object({
     id: z.string().uuid(),
-    teachers: z.array(z.object({id:z.string().uuid()}))
+    teachers: z.array(z.string().uuid())
 })
+
+export const evaluatorsList = z
+.array(
+    z.object({
+        name: z.string(),
+        id: z.string().uuid(),
+    })
+)
