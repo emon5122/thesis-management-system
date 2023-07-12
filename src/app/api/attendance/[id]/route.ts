8f9c7a882fe76b151943257ccbd5d5c106c6641f
Fabiha Khan <fabihakhan1999@gmail.com>
2023-07-12 17:20:50 +0600

    attendance done

--- src/app/api/attendance/[id]/route.ts
+++ src/app/api/attendance/[id]/route.ts
@@ -1,9 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { ParamsType } from "@/types/api";
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
-export const GET = async (req: any,{ params }: ParamsType) => {
+export const GET = async (req: NextRequest, { params }: ParamsType) => {
   const token = await getToken({ req });
   if (!token || !token?.sub || token?.role === "STUDENT") {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
@@ -11,10 +12,34 @@ export const GET = async (req: any,{ params }: ParamsType) => {
   try {
     const attendances = await prisma.attendance.findMany({
       where: {
-        studentId:params.id,
+        studentId: params.id,
       },
+      select: { id: true, weekNumber: true, createdAt: true },
     });
     return NextResponse.json(attendances);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+export const POST = async (req: NextRequest, { params }: ParamsType) => {
+  const token = await getToken({ req });
+  if (!token || !token?.sub || token?.role !== "TEACHER") {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const body = await req.json();
+  const validatedBody = z
+    .object({
+      weekNumber: z.number(),
+    })
+    .parse(body);
+
+  try {
+    const attendance = await prisma.attendance.create({
+      data: { weekNumber: validatedBody.weekNumber, studentId: params.id },
+    });
+    return NextResponse.json(attendance);
   } catch (e) {
     console.log(e);
   } finally {
--- src/app/dashboard/profile/[id]/page.tsx
+++ src/app/dashboard/profile/[id]/page.tsx
@@ -6,10 +6,19 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import { useQueries } from "@tanstack/react-query";
+import { useMutation, useQueries, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { getSession } from "next-auth/react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@mui/material";
+import { toast } from "react-toastify";
 
 const Profile = ({ params: { id } }: ParamsType) => {
+  const queryClient = useQueryClient();
+
   const [value, setValue] = useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -33,10 +42,53 @@ const Profile = ({ params: { id } }: ParamsType) => {
         queryKey: ["attendance", id],
         staleTime: 30000,
       },
+      {
+        queryFn: async () => {
+          return await getSession();
+        },
+        queryKey: ["session"],
+        staleTime: 60000,
+      },
     ],
   });
   const { data: user } = results[0];
   const { data: attendance } = results[1];
+  const { data: session } = results[2];
+
+  // attendance table
+  const columns: GridColDef[] = [
+    { field: "weekNumber", headerName: "Week Number", width: 130 },
+    {
+      field: "createdAt",
+      headerName: "Status",
+      width: 130,
+      valueGetter: (params: GridValueGetterParams) => {
+        {
+          return params.field ? "Present" : "Absent";
+        }
+      },
+    },
+  ];
+  const attendanceValidator = z.object({ weekNumber: z.number() });
+  type attendanceType = z.infer<typeof attendanceValidator>;
+  const form = useForm<attendanceType>({
+    resolver: zodResolver(attendanceValidator),
+    defaultValues: {
+      weekNumber: 0,
+    },
+  });
+  const attendanceMutation = useMutation({
+    mutationFn: async (data: attendanceType) => {
+      return await myAxios.post(`attendance/${id}`, { ...data });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["attendance", id]);
+      toast("Success");
+    },
+    onError: () => {
+      toast("error");
+    },
+  });
 
   const component = (
     <>
@@ -78,7 +130,50 @@ const Profile = ({ params: { id } }: ParamsType) => {
             </div>
           </TabPanel>
           <TabPanel value="2">
-            <div>{JSON.stringify(attendance)}</div>
+            <div className="flex flex-row justify-between gap-10">
+              <div style={{ height: 400, width: "100%" }}>
+                {attendance && (
+                  <DataGrid
+                    rows={attendance}
+                    columns={columns}
+                    initialState={{
+                      pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                      },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                  />
+                )}
+              </div>
+              {session?.user?.role === "TEACHER" && (
+                <form
+                  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                  onSubmit={form.handleSubmit((values: attendanceType) => {
+                    attendanceMutation.mutate(values);
+                    form.reset();
+                  })}
+                >
+                  <div className="mb-2">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="weekNumber"
+                    >
+                      Week Number
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="weekNumber"
+                      type="number"
+                      placeholder="Week Number"
+                      {...form.register("weekNumber", { valueAsNumber: true })}
+                    />
+                  </div>
+                  <Button variant="outlined" type="submit">
+                    Submit
+                  </Button>
+                </form>
+              )}
+            </div>
           </TabPanel>
           <TabPanel value="3">Task data</TabPanel>
         </TabContext>
